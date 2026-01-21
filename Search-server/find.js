55dd8d1e19061c3e5a3887b692c0f65c3b1117f7
Ohka_M. <masaruk967@gmail.com>
2026-01-22 07:59:54 +0900

    Create find.js

---
+++ Search-server/find.js
@@ -0,0 +1,23 @@
+/** @param {NS} ns **/
+export async function main(ns) {
+  const target = ns.args[0];
+  if (!target) {
+    ns.tprint("Usage: run path.js [server name]");
+    return;
+  }
+
+  let path = [target];
+
+  // Follow the parent tree from the target server until you reach 'home'
+  // The first element of ns.scan() is always the "parent server"
+  while (path[0] !== "home") {
+    let parent = ns.scan(path[0])[0];
+    path.unshift(parent);
+  }
+
+  // Display the connection command in the terminal
+  await ns.alert("\n" + path.map(server => `connect ${server}`).join("; "));
+}
+export function autocomplete(data, args) {
+  return data.servers;
+}
